b18eaa35265b1d603e2aecbcf43261eb7d8cbc9c
kirubelJ <146534846+kirubelJ@users.noreply.github.com>
2023-10-20 07:37:21 +0000

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -1,16 +1,24 @@
 let express = require('express');
 let app = express();
 
+//challenge
+
 console.log("Hello World");
+
+//challenge
 
 // app.get("/", function(req, res) {
 //   res.send("Hello Express")
 
 // });
 
+//challenge 
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+//challenge 3
 
 // Normal usage
 // app.use(express.static(__dirname + "/public"));
@@ -21,6 +29,10 @@ app.get("/", function(req, res) {
 
 app.use("/public", express.static(__dirname + "/public"));
 
+//Challenge 4
+app.get("/json", function(req,res){
+  res.json({"message": "Hello json"});
+})
 
 
 
