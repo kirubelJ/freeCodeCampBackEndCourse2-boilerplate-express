0fe0d18125fb56b81444f23c9e9757ab70803de0
kirubelJ <146534846+kirubelJ@users.noreply.github.com>
2023-10-20 07:04:39 +0000

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -3,11 +3,14 @@ let app = express();
 
 console.log("Hello World");
 
+// app.get("/", function(req, res) {
+//   res.send("Hello Express")
+
+// });
+
 app.get("/", function(req, res) {
-  res.send("Response String")
-
+  res.sendFile(__dirname + "/views/index.html");
 });
-
 
 
 
