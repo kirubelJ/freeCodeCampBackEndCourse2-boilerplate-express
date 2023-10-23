619ac5a86a639a73f5257150cb6c708e6fc1b303
kirubelJ <146534846+kirubelJ@users.noreply.github.com>
2023-10-23 04:46:30 +0000

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,24 +1,13 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
-//challenge
+//challenge 1
 
 console.log("Hello World");
+//
 
-//challenge
-
-// app.get("/", function(req, res) {
-//   res.send("Hello Express")
-
-// });
-
-//challenge 
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-//challenge 3
+//challenge 4
 
 // Normal usage
 // app.use(express.static(__dirname + "/public"));
@@ -26,13 +15,51 @@ app.get("/", function(req, res) {
 // Assets at the /public route
 // app.use("/public", express.static(__dirname + "/public"));
 
-
 app.use("/public", express.static(__dirname + "/public"));
+//
 
-//Challenge 4
-app.get("/json", function(req,res){
-  res.json({"message": "Hello json"});
+//challenge 7
+app.use(function middleware(req, res, next) {
+  // console.log(req.method);
+  // console.log(req.path);
+  // console.log(req.ip);
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+//
+
+//challenge 2
+
+// app.get("/", function(req, res) {
+//   res.send("Hello Express")
+
+// });
+//
+
+//challenge 3
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+//
+
+
+
+// //Challenge 5
+// app.get("/json", function(req,res){
+//   res.json({"message": "Hello json"});
+// })
+//
+
+//Challenge 6
+app.get("/json", function(req, res) {
+  if (process.env["MESSAGE_STYLE"] == "uppercase") {
+    res.json({ "message": "Hello json".toUpperCase() });
+  } else {
+    res.json({ "message": "Hello json" });
+  }
 })
+//
 
 
 
